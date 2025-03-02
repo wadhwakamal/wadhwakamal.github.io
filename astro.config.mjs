987e8297a61f4c8fe2d504449799efd638a61b28
Kamal Wadhwa <kamal@heady.io>
2025-03-02 22:50:19 +0530

    Added github action to deploy the website

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://kamal.github.io",
+  site: "https://wadhwakamal.github.io",
   integrations: [tailwind()],
 });
